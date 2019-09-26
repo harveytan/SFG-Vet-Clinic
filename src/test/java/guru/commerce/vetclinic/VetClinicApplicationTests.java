840551d4176e05c4ff9a251d54dd690cb6c02630
Harvey Tan <harvey2.tan@aol.com>
2019-09-26 12:39:03 -0400

    Initilized

---
+++ src/test/java/guru/commerce/vetclinic/VetClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package guru.commerce.vetclinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VetClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
