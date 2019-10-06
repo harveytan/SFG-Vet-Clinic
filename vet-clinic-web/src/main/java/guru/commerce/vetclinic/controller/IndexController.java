f204a894ab5f8dd71da13e091568efd87f57d547
Harvey Tan <harvey2.tan@aol.com>
2019-10-06 14:32:25 -0400

    create index page and controller. closes #8

---
+++ vet-clinic-web/src/main/java/guru/commerce/vetclinic/controller/IndexController.java
@@ -0,0 +1,13 @@
+package guru.commerce.vetclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"/", "", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
+}
