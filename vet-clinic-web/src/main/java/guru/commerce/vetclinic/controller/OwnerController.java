8909d7c05dd129fdebe9b3fee603decc3fa59103
Harvey Tan <harvey2.tan@aol.com>
2019-10-08 12:45:48 -0400

    Added function to list all owners. closes #21

--- vet-clinic-web/src/main/java/guru/commerce/vetclinic/controller/OwnerController.java
+++ vet-clinic-web/src/main/java/guru/commerce/vetclinic/controller/OwnerController.java
@@ -1,13 +1,25 @@
 package guru.commerce.vetclinic.controller;
 
+import guru.commerce.vetclinic.model.Owner;
+import guru.commerce.vetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        Owner owner = ownerService.findById(1L);
+        System.out.println(owner);
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
