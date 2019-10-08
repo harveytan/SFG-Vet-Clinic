799881863ab7699060f0b9a7c8cacb3e61c121fc
Harvey Tan <harvey2.tan@aol.com>
2019-10-08 12:56:48 -0400

    Added function to list all vets. closes #14

--- vet-clinic-data/src/main/java/guru/commerce/vetclinic/services/map/VetServiceMap.java
+++ vet-clinic-data/src/main/java/guru/commerce/vetclinic/services/map/VetServiceMap.java
@@ -1,8 +1,6 @@
 package guru.commerce.vetclinic.services.map;
 
-import guru.commerce.vetclinic.model.Owner;
 import guru.commerce.vetclinic.model.Vet;
-import guru.commerce.vetclinic.services.OwnerService;
 import guru.commerce.vetclinic.services.VetService;
 import org.springframework.stereotype.Service;
 
@@ -13,26 +11,27 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
     @Override
     public Vet save(Vet object) {
-        return null;
+        return super.save(object.getId(), object);
     }
 
     @Override
     public Set<Vet> findAll() {
-        return null;
+        return super.findAll();
     }
 
     @Override
     public Vet findById(Long id) {
-        return null;
+        return super.findById(id);
     }
 
     @Override
     public void delete(Vet object) {
+        super.delete(object);
 
     }
 
     @Override
     public void deleteById(Long id) {
-
+        super.deleteById(id);
     }
 }
